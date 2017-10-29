ca575a492781f8fb7973b15b89401ede0df77c2b
Himanshu Santolia <himanshusantolia@live.com>
2017-10-29 17:27:22 +0000

    grid applied

--- tictactoe.js
+++ tictactoe.js
@@ -21,7 +21,6 @@ function gameCode(){
 	var turn = "pehla" ; 
 	var matrix = [[-3,-3,-3],[-3,-3,-3],[-3,-3,-3]];
 
-
 	$("table tr").on("click",".check",function(event){
 	var pehla = $(this).find(".pehla");
 	var dusra = $(this).find(".dusra");
@@ -161,7 +160,6 @@ function playerWon(player){
 		}
 	}
 }
-
 
 // Winner check
 function checkWin(mat){
